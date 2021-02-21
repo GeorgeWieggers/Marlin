72c73611369c3e24a0735101bce977a0718d1dc3
George Wieggers <george.wieggers@gmail.com>
2021-02-21 21:56:13 +0100

    Preheat ABS

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1627,8 +1627,8 @@
 #define PREHEAT_1_FAN_SPEED     0 // Value from 0 to 255
 
 #define PREHEAT_2_LABEL       "ABS"
-#define PREHEAT_2_TEMP_HOTEND 240
-#define PREHEAT_2_TEMP_BED    100
+#define PREHEAT_2_TEMP_HOTEND 230
+#define PREHEAT_2_TEMP_BED     80
 #define PREHEAT_2_FAN_SPEED     0 // Value from 0 to 255
 
 /**
