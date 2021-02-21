7a1848a001b3e40a62926900e8790ac0f8c92253
George Wieggers <george.wieggers@gmail.com>
2021-02-21 19:24:13 +0100

    pid tuning - E3D hotend

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -499,9 +499,10 @@
     #define DEFAULT_Ki_LIST {   1.08,   1.08 }
     #define DEFAULT_Kd_LIST { 114.00, 114.00 }
   #else
-    #define DEFAULT_Kp  22.20
-    #define DEFAULT_Ki   1.08
-    #define DEFAULT_Kd 114.00
+    // E3D hotend
+    #define DEFAULT_Kp 17.35
+    #define DEFAULT_Ki 1.38
+    #define DEFAULT_Kd 54.74
   #endif
 #endif // PIDTEMP
 
