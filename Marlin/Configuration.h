c79660e80eae0f9ab6028c06e154ed782028d416
George Wieggers <george.wieggers@gmail.com>
2021-02-21 12:12:11 +0100

    DEFAULT_AXIS_STEPS_PER_UNIT

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -742,7 +742,7 @@
  * Override with M92
  *                                      X, Y, Z, E0 [, E1[, E2...]]
  */
-#define DEFAULT_AXIS_STEPS_PER_UNIT   { 80, 80, 4000, 500 }
+#define DEFAULT_AXIS_STEPS_PER_UNIT   { 79.6, 78.9, 400, 97.2 }
 
 /**
  * Default Max Feed Rate (mm/s)
